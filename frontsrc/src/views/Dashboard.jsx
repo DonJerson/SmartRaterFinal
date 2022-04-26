c873f116238f513358a9f7df42907a513dd6a0cf
prieto <jersonamendez@gmail.com>
2022-04-26 19:40:49 -0400

    Profile icon on logged

---
+++ frontsrc/src/views/Dashboard.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+  
+function Dashboard(props){
+    return(
+      <>
+      <div className="myRow center" style={{marginTop:"30px"}}>
+
+<div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+</div>
+      {props.children}
+      </>
+    )
+  }
+
+export default Dashboard;
