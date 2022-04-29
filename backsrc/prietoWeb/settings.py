62c0f0e6283dc83324f30a95729ba06c42709ba2
prieto <jersonamendez@gmail.com>
2022-04-28 23:26:16 -0400

    changes

--- backsrc/prietoWeb/settings.py
+++ backsrc/prietoWeb/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = '^$am8n_mab&2u^lobwr4gd9wwi8f04jg5frtil^0u_*z214%2('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['172.20.10.3','10.1.10.229','10.0.0.229','10.1.10.178','teteo.smartrater.us','smartrater.us','civiltools.club','costosback.herokuapp.com','inversionesmendez.herokuapp.com','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['172.20.10.3','10.1.10.229','10.0.0.229','10.1.10.178','teteo.smartrater.us','smartrater.us','civiltools.club','jersonmendez.com','inversionesmendez.herokuapp.com','127.0.0.1','localhost']
 
 # Application definition   
 CORS_ORIGIN_ALLOW_ALL=True
@@ -147,12 +147,12 @@ DATABASES = {
 'ENGINE': 'django.db.backends.postgresql_psycopg2',
 'NAME': 'smartrater',
 'USER': 'donjerson',
-'PASSWORD': 'W3lcome77',
-'HOST': 'postgres',
+'PASSWORD': 'baSzi3-fonqax-jygwad',
+'HOST': 'jersonmendez.com',
 'PORT': '5432'
 }}
 
-CORS_ORIGIN_WHITE_LIST = '172.20.10.3','10.1.10.229','10.0.0.229','10.1.10.178','teteo.smartrater.us','smartrater.us','civiltools.club','costosback.herokuapp.com','127.0.0.1:8080','localhost:8080',
+CORS_ORIGIN_WHITE_LIST = '172.20.10.3','10.1.10.229','10.0.0.229','10.1.10.178','teteo.smartrater.us','smartrater.us','civiltools.club','jersonmendez.com','127.0.0.1:8080','localhost:8080',
 
 JWT_AUTH = {
     'JWT_RESPONSE_PAYLOAD_HANDLER' :   'Insurance.utils.custom_jwt_response_handler',
