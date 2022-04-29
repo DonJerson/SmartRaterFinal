1d123d6eb174b046845e032da15669661d7e66e0
jersonRaspberry <jersonamendez@gmail.com>
2022-04-29 04:41:04 +0100

    niceJob

--- backsrc/production.sh
+++ backsrc/production.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 #!/bin/bash -e
-bash -c 'source /home/ubuntu/smartback/smartvenv/bin/activate &&
- cd /home/ubuntu/smartback && python manage.py runsslserver 0.0.0.0:8081
+bash -c 'source /home/ubuntu/smartrater/backsrc/smartvenv/bin/activate &&
+ cd /home/ubuntu/smartrater/backsrc && python manage.py runsslserver 0.0.0.0:8081
   --certificate /etc/letsencrypt/live/jersonmendez.com/fullchain.pem 
   --key /etc/letsencrypt/live/jersonmendez.com/privkey.pem'
