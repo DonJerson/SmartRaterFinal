8e2ff62851c3cc0208bf8523ef0999b69ae3965d
jersonA <jersonamendez@gmail.com>
2022-05-01 17:18:32 +0000

    ProductionServerin

--- backsrc/production.sh
+++ backsrc/production.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -e
 
-bash -c 'source /root/smartrater/backsrc/smartvenv/bin/activate &&
- cd /root/smartrater/backsrc && python manage.py runsslserver 0.0.0.0:8081
-  --certificate /etc/letsencrypt/live/jersonmendez.com/fullchain.pem 
-  --key /etc/letsencrypt/live/jersonmendez.com/privkey.pem'
+bash -c 'cd /root/smartrater/backsrc && /root/smartrater/backsrc/smartvenv/bin/gunicorn prietoWeb.wsgi:application --bind 0.0.0.0:443 --certfile /etc/letsencrypt/live/jersonmendez.com/fullchain.pem --keyfile /etc/letsencrypt/live/jersonmendez.com/privkey.pem'
+
+# bash -c 'source /root/smartrater/backsrc/smartvenv/bin/activate &&
+#  cd /root/smartrater/backsrc && /root/smartrater/backsrc/smartvenv/bin/gunicorn prietoWeb.wsgi:application --bind 0.0.0.0:443 --certfile /etc/letsencrypt/live/jersonmendez.com/fullchain.pem --keyfile /etc/letsencrypt/live/jersonmendez.com/privkey.pem'
+
 
 # bash -c 'source /root/smartrater/backsrc/smartvenv/bin/activate &&
 #  cd /root/smartrater/backsrc && python manage.py runserver 0.0.0.0:8081'
