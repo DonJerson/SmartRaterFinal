846c0fb90add6cbe4abee2ef08d1afd6f3c149f9
prieto <jersonamendez@gmail.com>
2022-05-01 11:28:46 -0400

    niceUp

--- backsrc/production.sh
+++ backsrc/production.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
-# bash -c 'source /root/smartrater/backsrc/smartvenv/bin/activate &&
-#  cd /root/smartrater/backsrc && python manage.py runsslserver 0.0.0.0:8081
-#   --certificate /etc/letsencrypt/live/jersonmendez.com/fullchain.pem 
-#   --key /etc/letsencrypt/live/jersonmendez.com/privkey.pem'
-
 bash -c 'source /root/smartrater/backsrc/smartvenv/bin/activate &&
- cd /root/smartrater/backsrc && python manage.py runserver 0.0.0.0:8081'
+ cd /root/smartrater/backsrc && python manage.py runsslserver 0.0.0.0:8081
+  --certificate /etc/letsencrypt/live/jersonmendez.com/fullchain.pem 
+  --key /etc/letsencrypt/live/jersonmendez.com/privkey.pem'
+
+# bash -c 'source /root/smartrater/backsrc/smartvenv/bin/activate &&
+#  cd /root/smartrater/backsrc && python manage.py runserver 0.0.0.0:8081'
