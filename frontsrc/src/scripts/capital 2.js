ba3404d8f6a8dcfe2a7a60b7cacfa41c94a6eabd
donjerson <admin@smartrater.us>
2023-02-03 20:05:03 -0500

    niceUp

---
+++ frontsrc/src/scripts/capital 2.js
@@ -0,0 +1,8 @@
+//Formula for compound interest
+function compoundInterest(principal, rate, time) {
+    return principal * Math.pow(1 + rate, time);
+    }
+//Amortization formula for monthly income investment compound interest
+function amortization(principal, rate, time) {
+
+}
